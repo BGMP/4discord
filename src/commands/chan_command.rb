c8f4d231b6d8896486c65f7fee50883cfe745d20
BGMP <jose@bgmp.cl>
2020-11-27 12:03:59 -0300

    Fix /chan replies working only on one channel

--- src/commands/chan_command.rb
+++ src/commands/chan_command.rb
@@ -5,9 +5,9 @@ class ChanCommand
   POST_EMBED_COLOUR  = "#9b1f1f"
   REPLY_EMBED_COLOUR = "#dd2929"
 
-  @latest = nil                        # Latest post randomly pulled by /chan
-
   def register(bot)
+
+    @latest_pulls = Hash.new         # Latest posts randomly pulled by /chan. { :channel_id => post } map
 
     bot.command(:chan,
                 :descritpion         => "Main command for getting random 4chan posts",
@@ -19,17 +19,20 @@ class ChanCommand
                 :rescue              => "An internal exception has occurred."
     ) do |event, board|
 
+      channel_id = event.channel.id.to_s
+      latest = @latest_pulls[channel_id]
+
       if board.nil?
         board = DEFAULT_BOARD
       elsif board.eql?("replies")
-        if @latest.nil?
+        if latest.nil?
           return "You must pull a random post first! Use /chan"
         else
 
-          replies = @latest["last_replies"]
+          replies = latest["last_replies"]
 
           if replies.empty?
-            bot.channel(event.channel.id).send_embed do |embed|
+            bot.channel(channel_id).send_embed do |embed|
               embed.description = "This post has no replies."
             end
           else
@@ -39,7 +42,7 @@ class ChanCommand
                              .gsub("{1}", reply["tim"].to_s)
                              .gsub("{2}", reply["ext"].to_s)
 
-              bot.channel(event.channel.id).send_embed do |embed|
+              bot.channel(channel_id).send_embed do |embed|
                 embed.title = "#{reply["name"]} No. #{reply["no"]}"
                 embed.colour = REPLY_EMBED_COLOUR
                 embed.description = Nokogiri::HTML(reply["com"]).text
@@ -61,13 +64,13 @@ class ChanCommand
       thread_number = page == 0 ? rand(0.. 9) + 1 : rand(0.. 9)
 
       post = ChanAPI.get_post(board, page, thread_number)
-      @latest = post
+      @latest_pulls[channel_id] = post
       file_url = API_MEDIA
                      .gsub("{0}", board.to_s)
                      .gsub("{1}", post["tim"].to_s)
                      .gsub("{2}", post["ext"].to_s)
 
-      bot.channel(event.channel.id).send_embed do |embed|
+      bot.channel(channel_id).send_embed do |embed|
         embed.title = "#{post["name"]} No. #{post["no"]}"
         embed.colour = POST_EMBED_COLOUR
         embed.description = Nokogiri::HTML(post["com"]).text
@@ -83,6 +86,8 @@ class ChanCommand
             :text => "API » Page #{page} » Thread #{thread_number}"
         )
       end
+
+      puts @latest_pulls
 
       if post["ext"].eql? ".webm"
         "#{file_url}"
