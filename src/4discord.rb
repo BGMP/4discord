7e65e5ea5e0f74908ff33fe02e53b187947056bc
José Benavente <jose@bgmp.cl>
2020-12-20 03:09:10 -0300

    Whoops, missed a few relative paths

--- src/4discord.rb
+++ src/4discord.rb
@@ -3,9 +3,9 @@ require 'nokogiri'
 require 'yaml'
 require 'sqlite3'
 
-require_relative '4chan_api'
-require_relative '4chan_boards'
-require_relative 'commands/framework/command_registry'
+require_relative './4chan_api'
+require_relative './4chan_boards'
+require_relative './commands/framework/command_registry'
 
 CONFIG = YAML.load_file("./../config/config.yml")
 
