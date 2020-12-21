40990061fbc4a7ee94847e67fae4b9bd836df358
BGMP <jose@bgmp.cl>
2020-12-21 06:05:13 -0300

    I give up with rake's embed sh sorcery

---
+++ mount.sh
@@ -0,0 +1,10 @@
+SCREEN_NAME=4discord
+
+screen -XS 4discord quit
+
+git reset --hard
+git clean -fd
+git pull
+bundle install
+
+screen -dmS $SCREEN_NAME bash -c 'cd src && ruby 4discord.rb' $SCREEN_NAME
