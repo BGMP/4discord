a199198b31f81dee11e77bfe3cdfbf88d68fa5fd
José Benavente <jose@bgmp.cl>
2021-02-04 02:37:25 -0300

    Enhance loggings & checks against 4chan's API

--- src/4chan_status.rb
+++ src/4chan_status.rb
@@ -1,14 +1,21 @@
+# A class to continuously check on 4chan's API status, and log changes to its responses
+#
+
+require 'discordrb'
 require 'net/http'
 
 require_relative '4chan_api'
 
 module ChanStatus
-  LOG_TIMESTAMP_FORMAT = '%Y-%m-%d %H:%M:%S.%L'
 
   class << self
     def run_active_checker
 
       Thread::new do
+
+        @a_status = "Not checked"
+        @i_status = "Not checked"
+
         while true
           post = ChanAPI.get_post("b", 0, 0)  # Pull a random post to later test against media
 
@@ -18,10 +25,20 @@ module ChanStatus
           a4cdn_response = Net::HTTP.get_response(a4cdn_uri)
           i4cdn_response = Net::HTTP.get_response(i4cdn_uri)
 
-          puts("[INFO : 4chan_api @ #{Time.now.strftime(LOG_TIMESTAMP_FORMAT)}] API endpoints returned status #{a4cdn_response.code} #{a4cdn_response.message}")
-          puts("[INFO : 4chan_api @ #{Time.now.strftime(LOG_TIMESTAMP_FORMAT)}] API media returned status #{i4cdn_response.code} #{i4cdn_response.message}")
+          a_status = "#{a4cdn_response.code} #{a4cdn_response.message}"
+          i_status = "#{i4cdn_response.code} #{i4cdn_response.message}"
 
-          sleep(600)  # Check the API status every 10 minutes.
+          if @a_status != a_status
+            Discordrb::LOGGER.info("API endpoints returned status #{a_status}. Previously: #{@a_status}")
+            @a_status = a_status
+          end
+
+          if @i_status != i_status
+            Discordrb::LOGGER.info("API media returned status #{i_status}. Previously: #{@i_status}")
+            @i_status = i_status
+          end
+
+          sleep(6)  # Check the API status every 10 minutes.
         end
       end
     end
