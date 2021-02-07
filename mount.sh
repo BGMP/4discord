9423bcbec8ddad4f7710b560b9e12b86c12a93ac
BGMP <bgmp0021@gmail.com>
2021-02-07 06:30:16 -0300

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,11 +1,5 @@
 SCREEN_NAME=4discord
 
 screen -XS $SCREEN_NAME quit
-
-git reset --hard
-git clean -fd
-git pull
-bundle install
-
 screen -dmS $SCREEN_NAME bash -c 'cd src && ruby 4discord.rb'
 echo "[SUCCESS] 4discord successfully mounted on screen" $SCREEN_NAME
