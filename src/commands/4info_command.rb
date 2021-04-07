775d542ee235b6450c28b207092611a76ea7f6c2
José Benavente <jose@bgmp.cl>
2021-04-07 13:17:54 -0400

    Remove /4info user restrictions

--- src/commands/4info_command.rb
+++ src/commands/4info_command.rb
@@ -5,14 +5,6 @@
 class BotInfoCommand
   EMBED_COLOUR = '#9b1f1f'
 
-  # Members of the 4discord staff team allowed to perform info requests.
-  STAFF = [
-    265233026072444929, # BGM
-    238736626262605824, # KingOfSquares
-    626141898146578433, # Teszeo
-    308254655148720131  # AbdulAkbar
-  ].freeze
-
   def register(bot)
     bot.command(:"4info",
                 :descritpion => 'Display general information and metrics for the bot.',
@@ -20,8 +12,6 @@ class BotInfoCommand
                 :min_args => 0,
                 :max_args => 0,
                 :rescue => 'An internal exception has occurred.') do |event|
-      sender_id = event.user.id
-      return unless STAFF.include?(sender_id)
 
       servers_added_to = bot.servers.size
 
